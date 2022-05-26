af8a05eb17b03c4bc5801ac19f99c0120ccc93ea
leogs2003 <leogs2003@gmail.com>
2022-05-26 20:18:30 -0300

    exercício JS

---
+++ script.js
@@ -0,0 +1,6 @@
+let nome = prompt(`digite seu nome`);
+let sobrenome = prompt(`digite seu sobrenome`);
+
+alert(`Nome completo: ${nome.toLocaleUpperCase()} ${sobrenome}`);
+
+alert(`Nome de catalogo: ${Sobrenome.toLocaleUpperCase()} ${nome}`);
